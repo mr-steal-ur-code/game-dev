ba2f91ebca6521627f7ef29eee19475c14cd3dde
CJ Karkut <cjkarkut@gmail.com>
2021-12-04 11:47:02 -0600

    stuck

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let cjsTank = document.querySelector('.tank');
+let moveBy = 10;
+
+window.addEventListener('onload' , () => {
+    cjsTank.style.position = 'absolute';
+    cjsTank.style.left = 0;
+    cjsTank.style.top = 0;
+});
+
+window.addEventListener('keydown' , (e) => {
+    
+    switch (e.key) {
+        case 37:
+            cjsTank.style.left = parseInt(cjsTank.style.left) - moveBy + 'px';
+            break;
+        case 39:
+            cjsTank.style.left = parseInt(cjsTank.style.left) + moveBy + 'px';
+            break;
+        case 38:
+            cjsTank.style.top = parseInt(cjsTank.style.left) - moveBy + 'px';
+            break;
+        case 40:
+            cjsTank.style.top = parseInt(cjsTank.style.left) + moveBy + 'px';
+            break;
+    }
+});
